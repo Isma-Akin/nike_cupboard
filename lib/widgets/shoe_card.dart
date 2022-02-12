c22878bd4c837b8be5561f9d6c220fd0f69fd454
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-12 19:07:58 +0000

    refactor(shoe_card): fix syntax | add missing import | add expanded widget with children

--- lib/widgets/shoe_card.dart
+++ lib/widgets/shoe_card.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 import 'package:nike_cupboard/data/data.dart';
 
 class ShoeCard extends StatelessWidget {
   final ShoeDataModel shoe;
 
-  const ShoeCard({required Key key, required this.shoe}): super(key: key);
+  const ShoeCard({key, required this.shoe}): super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -28,6 +29,32 @@ class ShoeCard extends StatelessWidget {
                   child: Image.asset(shoe.image),
                 ),
             )],
+          )),
+          Expanded(child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              RichText(text:
+              TextSpan(
+                text: shoe.name,
+                style: Theme.of(context).textTheme.headline4,
+                children: <TextSpan> [
+                          TextSpan(
+                            text: '\nby Nike',
+                            style: Theme.of(context).textTheme.caption)
+                ],
+              )
+              ),
+              Text(shoe.tagLine,
+                style: Theme.of(context).textTheme.caption,),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Text("\$\ ${shoe.price}",
+                    style: Theme.of(context).textTheme.headline4,)
+                ],
+              )
+            ],
           ))
         ],
       )
