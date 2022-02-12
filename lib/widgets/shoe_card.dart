2b1946b9c6ffdfc2d2a85d085d424227a55a9955
Isma-Akin <aakinsemoyin@hotmail.com>
2022-02-12 18:42:01 +0000

    feat(shoe_card): add ShoeCard stateless widget

---
+++ lib/widgets/shoe_card.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/cupertino.dart';
+import 'package:nike_cupboard/data/data.dart';
+
+class ShoeCard extends StatelessWidget {
+  final ShoeDataModel shoe;
+
+  const ShoeCard({required Key key, required this.shoe}): super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
